c30b56cd7dcd10376730787cc884f6fdf93d4a5b
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2025-06-19 16:37:58 -0700

    Update container-compose.rb

--- Formula/container-compose.rb
+++ Formula/container-compose.rb
@@ -1,8 +1,8 @@
 // container-compose.rb
 class Container-Compose < Formula
-  desc "Manage the local dictionary on Mac."
-  homepage "https://github.com/Mcrich23/Container-Compose"
-  url "https://github.com/Mcrich23/Container-Compose.git", tag: "0.1.0"
+  desc "Manage Apple Container with Docker Compose files."
+  homepage "https://github.com/Mcrich23/container-compose"
+  url "https://github.com/Mcrich23/container-compose.git", tag: "0.1.0"
   version "0.1.0"
 
   depends_on "xcode": [:build]
