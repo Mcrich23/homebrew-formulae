024ce469852dea5be9352cbb03ffdb0db2dadc26
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2025-07-09 00:21:12 -0700

    Update container-compose.rb

--- Formula/container-compose.rb
+++ Formula/container-compose.rb
@@ -1,8 +1,8 @@
 class ContainerCompose < Formula
   desc "Manage Apple Container with Docker Compose files."
   homepage "https://github.com/Mcrich23/container-compose"
-  url "https://github.com/Mcrich23/container-compose/releases/download/0.1.1/container-compose-macos-arm64"
-  sha256 "49314f79767afad4f835b516aa3774e2e18c66db1d062113c4708736c61f5eac"
+  url "https://github.com/Mcrich23/container-compose/releases/download/0.2.0/container-compose-macos-arm64"
+  sha256 "3a0a52a0cac6729a71ec2d7aaaf376b22a92e4032935f1aa04e5a212b7e2cf83"
   version "0.1.1"
 
   def install
