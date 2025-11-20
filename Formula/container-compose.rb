614d76b988c3ed6b4b380d7ea5a3f05e21fc720b
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2025-11-20 10:37:00 -0800

    Update container-compose.rb

--- Formula/container-compose.rb
+++ Formula/container-compose.rb
@@ -2,10 +2,6 @@ class ContainerCompose < Formula
   desc "Manage Apple Container with Docker Compose files"
   homepage "https://github.com/mcrich23/container-compose"
 
-  # Use the universal (multi-arch) binary
-  url "https://github.com/Mcrich23/container-compose/releases/download/0.6.0/container-compose"
-  sha256 "da835878b1ff4309c587f82e97f2280fcf27d21e8256da657f14b70ea3939096"
-
   # License is MIT
   license "MIT"
 
